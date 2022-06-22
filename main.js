dc6e1430a60fd68414295dde6deafd5c36f05a83
sublimeb0y <mateuselpidio0@gmail.com>
2022-06-22 02:17:34 -0300

    Comentarios finalizados e variavel inutil removida

--- main.js
+++ main.js
@@ -27,28 +27,34 @@ document
         if (inputName === alunos.aluno1.nome) {
             //Se o valor inserido for "Mateus" vou passar os parametros pra funçao de calculo
             calc(alunos.aluno1.nota1, alunos.aluno1.nota2, alunos.aluno1.nome);
-        } //Checando se o valor inserido na inputbox é igual a "Mateus"
+        } //Checando se o valor inserido na inputbox é igual a "Leonardo"
         else if (inputName === alunos.aluno2.nome) {
             //Se o valor inserido for "Leonardo" vou passar os parametros pra funçao de calculo
             calc(alunos.aluno2.nota1, alunos.aluno2.nota2, alunos.aluno2.nome);
-        } //Checando se o valor inserido na inputbox é igual a "Leonardo"
+        } //Checando se o valor inserido na inputbox é igual a "Ferreira"
         else if (inputName === alunos.aluno3.nome) {
-            //Se o valor inserido for "Leonardo" vou passar os parametros pra funçao de calculo
+            //Se o valor inserido for "Ferreira" vou passar os parametros pra funçao de calculo
             calc(alunos.aluno3.nota1, alunos.aluno3.nota2, alunos.aluno3.nome);
         } 
         else {
+            //Se o valor inserido nao for nenhum dos citados acima vai aparecer uma alerta requisitando um valor valido
             alert("INSIRA UM NOME VALIDO");
         }
+        //Chamando a funçao de calculo pra ela ocorrer no click também
         calc();
     });
 
+//funçao que vai calcular a média da nota do aluno usando os parametros dados nas checagens anteriores
 function calc(nota1, nota2, aluno) {
+    //formula que vai usar os dados pra calcular a média
     const total = (nota1 + nota2) / 2;
+    //variavel que vai receber um texto seguido do resultado da formula "total"
     let resultado = "sua nota total foi " + total;
-    const notaTotal = total;
+    //Textos usando alguns dos dados coletados pelas variaveis
     const parabens = "Parabens " + aluno + ", " + resultado;
     const reprovado = "Voce foi reprovado," + aluno + " ,boa sorte na proxima";
 
+    //If statement que checa se a nota foi abaixo ou acima da média 6
     if (total >= 6.0) {
         createElement(parabens);
     } else if (total <= 5.9) {
@@ -56,6 +62,7 @@ function calc(nota1, nota2, aluno) {
     }
 }
 
+//Funçao que vai criar o elemento onde o resultado vai ser mostrado (</p>) e também vai fazer a quebra de linhas (<br>)
 function createElement(resultado) {
     let text = document.createTextNode(resultado);
     let br = document.createElement("br");
